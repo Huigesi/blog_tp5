20bc868b405a36ae8e5f670b005c07771366215a
Huigesi <791339970@qq.com>
2019-03-21 18:04:33 +0800

    21

--- application/admin/controller/Article.php
+++ application/admin/controller/Article.php
@@ -1,77 +1,117 @@
 <?php
+
 namespace app\admin\controller;
+
 use think\Controller;
 use app\Admin\model\Article as ArticleModel;
 use app\admin\controller\Base;
+
 class Article extends Controller
 {
     public function lst()
     {
-    	$list = ArticleModel::paginate(3);
-    	$this->assign('list',$list);
+        $list = ArticleModel::paginate(3);
+        /*$list = db('article')->alias('a')->join('cate c', 'c.id=a.cateid')
+            ->field('a.id, a.title,a.pic, a.author, a.state, c.catename')
+            ->paginate(3);*/
+        $this->assign('list', $list);
         return $this->fetch();
     }
 
     public function add()
-    {	
-    	if(request()->isPost()){
+    {
+        if (request()->isPost()) {
+            //dump($_POST);die;
+            $data = [
+                'title' => input('title'),
+                'author' => input('author'),
+                'desc' => input('desc'),
+                'keywords' => input('keywords'),
+                'content' => input('content'),
+                'cateid' => input('cateid'),
+                'time' => time(),
+            ];
+            if (input('state') == 'on') {
+                $data['state'] = 1;
+            }
+            if (isset($_FILES['pic']['tmp_name'])) {
+                if ($_FILES['pic']['tmp_name']) {
+                    $file = request()->file('pic');
+                    $info = $file->move(ROOT_PATH . 'public' . DS . 'static/uploads');
+                    $data['pic'] = '/static/uploads/' . $info->getSaveName();
+                }
+        }
 
-			$data=[
-    			'title'=>input('title'),
-                'url'=>input('url'),
-    			'desc'=>input('desc'),
-    		];
-    		$validate = \think\Loader::validate('Article');
-    		if(!$validate->scene('add')->check($data)){
-			   $this->error($validate->getError()); die;
-			}
-    		if(db('Article')->insert($data)){
-    			return $this->success('添加文章成功！','lst');
-    		}else{
-    			return $this->error('添加文章失败！');
-    		}
-    		return;
-    	}
+            $validate = \think\Loader::validate('Article');
+            if (!$validate->scene('add')->check($data)) {
+                $this->error($validate->getError());
+                die;
+            }
+            if (db('Article')->insert($data)) {
+                return $this->success('添加文章成功！', 'lst');
+            } else {
+                return $this->error('添加文章失败！');
+            }
+            return;
+        }
         $cateres = db('cate')->select();
         $this->assign('cateres', $cateres);
         return $this->fetch();
     }
 
-    public function edit(){
-    	$id=input('id');
-    	$articles=db('article')->find($id);
-    	if(request()->isPost()){
-    		$data=[
-    			'id'=>input('id'),
-                'title'=>input('title'),
-                'url'=>input('url'),
-    			'desc'=>input('desc'),
-    		];
-			$validate = \think\Loader::validate('Article');
-    		if(!$validate->scene('edit')->check($data)){
-			   $this->error($validate->getError()); die;
-			}
-    		if(db('article')->update($data)){
-    			$this->success('修改文章成功！','lst');
-    		}else{
-    			$this->error('修改文章失败！');
-    		}
-    		return;
-    	}
-    	$this->assign('articles',$articles);
-    	return $this->fetch();
+    public function edit()
+    {
+        $id = input('id');
+        $articles = db('article')->find($id);
+        if (request()->isPost()) {
+            $data = [
+                'id' => input('id'),
+                'title' => input('title'),
+                'author' => input('author'),
+                'desc' => input('desc'),
+                'keywords' => input('keywords'),
+                'content' => input('content'),
+                'cateid' => input('cateid'),
+            ];
+            if (input('state') == 'on') {
+                $data['state'] = 1;
+            }
+
+            if ($_FILES['pic']['tmp_name']) {
+                unlink(ROOT_PATH."/public".$articles['pic']);
+               //dump(SITE_URL.'/public'.$articles['pic']); die;
+                $file = request()->file('pic');
+                $info = $file->move(ROOT_PATH . 'public' . DS . 'static/uploads');
+                $data['pic'] = '/static/uploads/' . $info->getSaveName();
+            }
+            $validate = \think\Loader::validate('Article');
+            if (!$validate->scene('edit')->check($data)) {
+                $this->error($validate->getError());
+                die;
+            }
+            if (db('article')->update($data)) {
+                $this->success('修改文章成功！', 'lst');
+            } else {
+                $this->error('修改文章失败！');
+            }
+            return;
+        }
+        $this->assign('articles', $articles);
+        $cateres = db('cate')->select();
+        $this->assign('cateres', $cateres);
+        return $this->fetch();
     }
 
-    public function del(){
-    	$id=input('id');
-		if(db('Article')->delete(input('id'))){
-			$this->success('删除文章成功！','lst');
-		}else{
-			$this->error('删除文章失败！');
-		}
-    	
-    }
+    public function del()
+    {
+        $id = input('id');
+        if (db('Article')->delete(input('id'))) {
+            $this->success('删除文章成功！', 'lst');
+        } else {
+            $this->error('删除文章失败！');
+        }
 
+    }
 
 
 }
